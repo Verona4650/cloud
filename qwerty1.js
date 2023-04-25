a0edd97d7830cde5e13d7a3166353234f103a7d8
Ksu <Zayksenia2007@gmail.com>
2023-04-25 19:37:32 +0300

    Я написала своё имя

---
+++ qwerty1.js
@@ -0,0 +1,116 @@
+function f1(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'rgb(171, 49, 49)';
+    let text = document.querySelector('#fone');
+    text.textContent = 'красный';
+
+
+
+}
+function f2(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'rgb(45, 54, 179)';
+    let text = document.querySelector('#fone');
+    text.textContent = 'синий';
+    
+
+}
+function f3(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'lightblue';
+    let text = document.querySelector('#fone');
+    text.textContent = 'голубой';
+
+}
+function f4(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'green';
+    let text = document.querySelector('#fone');
+    text.textContent = 'зелёный';
+
+}
+function f5(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'gold';
+    let text = document.querySelector('#fone');
+    text.textContent = 'жёлтый';
+
+}
+function f6(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'brown';
+    let text = document.querySelector('#fone');
+    text.textContent = 'коричневый';
+
+}
+function f7(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'orange';
+    let text = document.querySelector('#fone');
+    text.textContent = 'оранжевый';
+
+}
+function f8(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'black';
+    let text = document.querySelector('#fone');
+    text.textContent = 'чёрный';
+
+}
+function f9(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'white';
+    let text = document.querySelector('#fone');
+    text.textContent = 'белый';
+
+}
+function f10(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'pink';
+    let text = document.querySelector('#fone');
+    text.textContent = 'розовый';
+
+}
+function f11(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'violet';
+    let text = document.querySelector('#fone');
+    text.textContent = 'фиолетовый';
+
+}
+function f12(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'lightgreen';
+    let text = document.querySelector('#fone');
+    text.textContent = 'светло-зелёный';
+
+}
+function f13(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'beige';
+    let text = document.querySelector('#fone');
+    text.textContent = 'бежевый';
+
+}
+function f14(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'rgb(203, 147, 196)';
+    let text = document.querySelector('#fone');
+    text.textContent = 'лавандовый';
+
+}
+function f15(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'purple';
+    let text = document.querySelector('#fone');
+    text.textContent = 'малиновый';
+
+}
+function f16(){
+    let backcolor = document.querySelector('#fone');
+    backcolor.style.background = 'gray';
+    let text = document.querySelector('#fone');
+    text.textContent = 'серый';
+
+}
+
